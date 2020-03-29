1aca1996508a7ec5838f1f710610150f5b13aad1
batjargald <developerjagaa@gmail.com>
2020-03-29 16:03:42 +0100

    sunday work

--- src/bracket.js
+++ src/bracket.js
@@ -10,27 +10,33 @@ const playerToLevel = {
     64: 7,
     128: 8,
 }
+
+const UPPER = 1;
+const LOWER = 0;
 class Bracket extends Component {
     state = {
         userCount: 16,
-        users: [
-            { id: 1, userName: "user 1", side: 1, level: 5, seq: 1 },
-            { id: 2, userName: "user 2", side: 1, level: 5, seq: 1 },
-            { id: 3, userName: "user 3", side: 1, level: 5, seq: 1 },
-            { id: 4, userName: "user 4", side: 1, level: 5, seq: 1 },
-            { id: 5, userName: "user 5", side: 1, level: 5, seq: 1 },
-            { id: 6, userName: "user 6", side: 1, level: 5, seq: 1 },
-            { id: 7, userName: "user 7", side: 1, level: 5, seq: 1 },
-            { id: 8, userName: "user 8", side: 1, level: 5, seq: 1 },
-            { id: 9, userName: "user 9", side: 1, level: 5, seq: 1 },
-            { id: 10, userName: "user 10", side: 1, level: 5, seq: 1 },
-            { id: 11, userName: "user 11", side: 1, level: 5, seq: 1 },
-            { id: 12, userName: "user 12", side: 1, level: 5, seq: 1 },
-            { id: 13, userName: "user 13", side: 1, level: 5, seq: 1 },
-            { id: 14, userName: "user 14", side: 1, level: 5, seq: 1 },
-            { id: 15, userName: "user 15", side: 1, level: 5, seq: 1 },
-            { id: 16, userName: "user 16", side: 1, level: 5, seq: 1 },
-        ]
+        users: {
+            1: { id: 1, userName: "user 1", competitor: [2], side: [UPPER, UPPER], level: [4, 5], seq: [1, 1] },
+            2: { id: 2, userName: "user 2", competitor: [1], side: [UPPER], level: [5], seq: [2] },
+            3: { id: 3, userName: "user 3", competitor: [4], side: [UPPER], level: [5], seq: [3] },
+            4: { id: 4, userName: "user 4", competitor: [3], side: [UPPER], level: [5], seq: [4] },
+            5: { id: 5, userName: "user 5", competitor: [6], side: [UPPER], level: [5], seq: [5] },
+            6: { id: 6, userName: "user 6", competitor: [5], side: [UPPER], level: [5], seq: [6] },
+            7: { id: 7, userName: "user 7", competitor: [8], side: [UPPER], level: [5], seq: [7] },
+            8: { id: 8, userName: "user 8", competitor: [7], side: [UPPER], level: [5], seq: [8] },
+            9: { id: 9, userName: "user 9", competitor: [10], side: [UPPER], level: [5], seq: [9] },
+            10: { id: 10, userName: "user 10", competitor: [9], side: [UPPER], level: [5], seq: [10] },
+            11: { id: 11, userName: "user 11", competitor: [12], side: [UPPER], level: [5], seq: [11] },
+            12: { id: 12, userName: "user 12", competitor: [11], side: [UPPER], level: [5], seq: [12] },
+            13: { id: 13, userName: "user 13", competitor: [14], side: [UPPER], level: [5], seq: [13] },
+            14: { id: 14, userName: "user 14", competitor: [13], side: [UPPER], level: [5], seq: [14] },
+            15: { id: 15, userName: "user 15", competitor: [16], side: [UPPER], level: [5], seq: [15] },
+            16: { id: 16, userName: "user 16", competitor: [15], side: [UPPER], level: [5], seq: [16] },
+        },
+        matches: {
+            151: { winner: 1, loser: 2, scores: { r1: { 1: 16, 2: 10 } }, bo: 1, streams: {}, matchDate: null, statLink: null }
+        },
     }
 
     // componentDidMount() {
@@ -39,8 +45,66 @@ class Bracket extends Component {
     //         .then(users => this.setState({ users }))
     // }
 
-    updateUser(user) {
-        console.log("updateUser", user);
+    updateUser(user_id) {
+        let { users, matches } = this.state;
+        let user = users[user_id]
+        let competitor = users[user.competitor[0]];
+        console.log("competitor ", competitor);
+
+        if (typeof competitor === "undefined") {
+            return;
+        }
+        if (user.side) { //is upper bracket
+            const matchKey = parseInt(user.side[0].toString() +
+                user.level[0].toString() +
+                Math.ceil(user.seq[user.seq.length - 1] / 2));
+            //set next upper bracket next level
+            user.level.unshift(user.level[0] - 1);
+            //next time play on upper
+            user.side.unshift(UPPER);
+            //sequence
+            const usersNextSeq = Math.ceil(user.seq[user.seq.length - 1] / 2);
+            const nextCompetitor = this.findCompetitor(usersNextSeq)
+            user.seq.push(usersNextSeq);
+            if (nextCompetitor) user.competitor.unshift(nextCompetitor);
+
+            //drop competitor to lower
+            competitor.side.unshift(LOWER);
+            const competitorsLowerSeq = Math.ceil(competitor.seq[competitor.seq.length - 1] / 2);
+            competitor.seq.push(competitorsLowerSeq);
+            const nextLowerCompetitor = this.findCompetitor(usersNextSeq)
+            if (nextLowerCompetitor) competitor.competitor.unshift(nextLowerCompetitor);
+
+            //update match
+            matches[matchKey] = {
+                winner: user_id,
+                loser: competitor.id,
+                scores: { r1: { 1: 16, 2: 10 } },
+                bo: 1,
+                streams: {},
+                matchDate: null,
+                statLink: null
+            };
+        } else {  // lower bracket
+
+        }
+        users[user_id] = user;
+        this.setState({ users, matches });
+
+        // console.log("users ", users);
+        // console.log("matches ", matches);
+
+    }
+
+    findCompetitor(level, seq) {
+        const { users } = this.state;
+        const filteredUser = Object.keys(users).filter(key => {
+            const thisSeq = (seq % 2 == 0) ? seq - 1 : seq + 1;
+            // users[key].seq[0]
+            return users[key].level[0] === level && thisSeq === users[key].seq[users[key].seq.length - 1]
+        });
+        if (filteredUser.length === 0) return null;
+        return filteredUser[0].id;
     }
 
     getColumns() {
@@ -49,9 +113,16 @@ class Bracket extends Component {
         const { users } = this.state;
         let slotsize = 16;
         for (let index = 0; index < columnCount; index++) {
-            columns.push(<Column key={index} users={users} slotSize={slotsize} handleUpdate={(user) => this.updateUser(user)} />)
+            columns.push(
+                <Column
+                    key={index}
+                    users={users}
+                    level={index}
+                    slotSize={slotsize}
+                    handleUpdate={(user_id, slotSize) => this.updateUser(user_id, slotSize)}
+                />
+            )
             slotsize = slotsize / 2;
-            console.log(slotsize);
         }
 
         return columns;
--- src/levelcolumn.jsx
+++ src/levelcolumn.jsx
@@ -13,20 +13,34 @@ class Column extends Component {
     state = {
         users: this.props.users,
         slotSize: this.props.slotSize,
+        level: this.props.level,
     }
 
     clickedUser(user) {
-        this.props.handleUpdate(user)
+        if (user)
+            this.props.handleUpdate(user.id, this.state.slotSize)
     }
 
-    getItems() {
-        const { users, slotSize } = this.state;
-        let items = []
+    getUpper() {
+        const { users, slotSize, level } = this.state;
+        // tuhain tire t oroh usersiig shuuj avah
+        const filteredUser = Object.keys(users).filter(key => {
+            return typeof users[key].level[level] !== "undefined";
+        });
+        // console.log("filteredUser ", filteredUser);
+
+        let items = [];
         for (let index = 0; index < slotSize; index++) {
-            items.push(<input
-                onClick={() => this.clickedUser(users[index])}
+            const currenUser = filteredUser.filter(key => {
+                return users[key].seq[level] === (index + 1) && users[key].side[level] === 1;
+            });
+            items.push(<div
+                onClick={() => this.clickedUser(users[currenUser[0]])}
+                type="text"
                 key={index}
-                value={users[index].userName} />)
+                className="node">
+                {typeof users[currenUser[0]] !== "undefined" ? users[currenUser[0]].userName : ""}
+            </div>)
         }
         return items;
     }
@@ -37,7 +51,7 @@ class Column extends Component {
         // );
         return (
             <div className="column">
-                {this.getItems()}
+                {this.getUpper()}
             </div>
         );
     }
